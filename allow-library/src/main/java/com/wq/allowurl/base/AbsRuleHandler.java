5761530ff880ae06ca7fe1a310dfca6604bff29b
wq <wangqi7676@163.com>
2018-03-06 14:17:35 +0800

    fix 泛型

--- allow-library/src/main/java/com/wq/allowurl/base/AbsRuleHandler.java
+++ allow-library/src/main/java/com/wq/allowurl/base/AbsRuleHandler.java
@@ -2,12 +2,14 @@ package com.wq.allowurl.base;
 
 import com.wq.allowurl.rule.IRuleHandle;
 
+import java.io.Serializable;
+
 /**
  * Create by wq on 2018/1/11.
  */
 
 @SuppressWarnings("all")
-public abstract class AbsRuleHandler<P> implements IRuleHandle {
+public abstract class AbsRuleHandler<P,V extends Serializable> implements IRuleHandle<V> {
     private P mParams;    // 需要去获取凭证的参数
 
     public AbsRuleHandler(P params) {
