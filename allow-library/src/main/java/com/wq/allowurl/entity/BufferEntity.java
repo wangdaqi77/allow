15c614740a84b7048c08aef733551d51114108ac
wangdaqi77 <462929012@qq.com>
2018-04-09 11:30:33 +0800

    Update BufferEntity.java
    fix

--- allow-library/src/main/java/com/wq/allowurl/entity/BufferEntity.java
+++ allow-library/src/main/java/com/wq/allowurl/entity/BufferEntity.java
@@ -3,19 +3,19 @@ package com.wq.allowurl.entity;
 import com.wq.allowurl.callback.OnAllowValueCallBack;
 
 import java.io.Serializable;
-import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 
 /**
  * Create by wq on 2018/1/11.
  */
 
 public class BufferEntity<T, V extends Serializable> {
-    private SoftReference<T> target = null;
-    private SoftReference<OnAllowValueCallBack<T, V>> callback;
+    private WeakReference<T> target = null;
+    private WeakReference<OnAllowValueCallBack<T, V>> callback;
 
     public BufferEntity(T target, OnAllowValueCallBack<T, V> callback) {
-        this.target = new SoftReference<>(target);
-        this.callback = new SoftReference<>(callback);
+        this.target = new WeakReference<>(target);
+        this.callback = new WeakReference<>(callback);
     }
 
     public void success(V allowValue) {
